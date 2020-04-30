db20b06ced235784e2395827fcc650c85097cafd
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-30 14:54:17 -0400

    feat: Set the initial app structure.

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App-container'>
+      <div className='App-logo'>component layout</div>
+    </div>
+  );
+}
+
+export default App;
