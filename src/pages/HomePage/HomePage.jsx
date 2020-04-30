24d379f1ccbdf10b9b97ffad87abcc8239566ee2
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-30 17:40:33 -0400

    feat: Create HomePage component.

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+      <div className='home-page__header'> header </div>
+      <div className='home-page__menu'> menu </div>
+      <div className='home-page__shopping-cart'> shopping cart </div>
+    </div>
+  );
+};
+
+export default HomePage;
