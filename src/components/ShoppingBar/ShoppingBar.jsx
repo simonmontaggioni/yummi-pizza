3bdad1381384911cca4ce3b7af7322a2145cb445
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-01 00:33:43 -0400

    feat: Improve structure for ShoppingBar component.

--- src/components/ShoppingBar/ShoppingBar.jsx
+++ src/components/ShoppingBar/ShoppingBar.jsx
@@ -4,11 +4,25 @@ import './ShoppingBar.css';
 const ShoppingBar = () => {
   return (
     <div className='shopping-bar'>
-      <div className='shopping-bar__cart'> cart </div>
-      <div className='shopping-bar__item-list'> items list</div>
-      <div className='shopping-bar__total-amount'>total</div>
-      <div className='shopping-bar__payment-button'>
-        <button>go to pay</button>
+      <div className='shopping-bar__cart'>
+        <span className='cart-badge'>0</span>
+      </div>
+      <div className='shopping-bar__item-list'>
+        <div>items list</div>
+      </div>
+      <div className='shopping-bar__total-amount'>
+        <div className='total-amount__title'>
+          <span>total</span>
+        </div>
+        <div className='total-amount__price'>
+          <div>
+            <span className='total-amount__coin'>$</span>
+            <span className='total-amount__cost'>45</span>
+          </div>
+        </div>
+      </div>
+      <div className='shopping-bar__payment'>
+        <button className='shopping-bar__payment-button'>go to pay</button>
       </div>
     </div>
   );
