4bfa8dd191e27860a2b497d0931de6188a2d90d0
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-01 15:20:02 -0400

    style: Set styles for Header component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,11 @@ const Header = () => {
   return (
     <header className='header'>
       <nav className='header__nav'>
-        <div className='nav__title'>title</div>
+        <div className='nav__title'>
+          <span>The Yummi Pizza</span>
+        </div>
         <div className='nav__item'>login</div>
-        <div className='nav__item'>signup</div>
+        <div className='nav__item nav__item--middle'>signup</div>
         <div className='nav__item'>coin</div>
       </nav>
     </header>
