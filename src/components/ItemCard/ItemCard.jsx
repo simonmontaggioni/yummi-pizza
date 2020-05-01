842f3a8315dc873b474307a3c297289c028341b1
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-30 23:16:34 -0400

    feat: Create ItemCard component and put it into Menu component.

---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ItemCard.css';
+import pizza from '../../assets/static/images/pizza-1.png';
+
+const ItemCard = (props) => {
+  return (
+    <div className='item-card'>
+      <div className='item-card__price'>
+        <span className='item-card__price--coin'>$</span>
+        <span className='item-card__price--cost'>9,99</span>
+      </div>
+      <div className='item-card__image'>
+        <img src={pizza} alt='pizza' />
+      </div>
+      <div className='item-card__title'>
+        <span>{props.title}</span>
+      </div>
+      <div className='item-card__footer'>
+        <button className='item-card__button'>
+          <span className='button__title'>Add to cart</span>
+          <span className='button-badge'>0</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCard;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Menu.css';
+import ItemCard from '../ItemCard/ItemCard';
 
 const Menu = () => {
   return (
@@ -9,14 +10,30 @@ const Menu = () => {
         <div className='menu__title'> Choose your favorite flavour</div>
         <div className='menu__slider'>
           <ul className='menu__list'>
-            <li className='menu__list--item'> pizza 1</li>
-            <li className='menu__list--item'> pizza 2</li>
-            <li className='menu__list--item'> pizza 3</li>
-            <li className='menu__list--item'> pizza 4</li>
-            <li className='menu__list--item'> pizza 5</li>
-            <li className='menu__list--item'> pizza 6</li>
-            <li className='menu__list--item'> pizza 7</li>
-            <li className='menu__list--item'> pizza 8</li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 1' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 2' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 3' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 4' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 5' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 6' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 7' />
+            </li>
+            <li className='menu__list--item'>
+              <ItemCard title='pizza 8' />
+            </li>
           </ul>
         </div>
       </div>
