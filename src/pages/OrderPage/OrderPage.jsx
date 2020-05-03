a9324b78d26e9f69db74d299cdaadfe82440178e
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-03 02:51:27 -0400

    feat: Add the navigation button to the OrderPage.

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -3,6 +3,7 @@ import './OrderPage.css';
 import Header from '../../components/Header/Header';
 import CartDetail from '../../components/CartDetail/CartDetail';
 import Billing from '../../components/Billing/Billing';
+import { Link } from 'react-router-dom';
 
 const OrderPage = () => {
   return (
@@ -12,6 +13,9 @@ const OrderPage = () => {
       </div>
       <div className='order-page__title'>
         <span>Cart Details</span>
+        <Link to='/home'>
+          <button className='back-button'>go back</button>
+        </Link>
       </div>
       <div className='order-page__main'>
         <div className='order-page__cart'>
