63def925cd421c18606431bc3019654cb0e9d2e5
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-03 02:06:37 -0400

    feat: Put DeliveryForm into Billing, Add flip effect to the Billing.

--- src/components/Billing/Billing.jsx
+++ src/components/Billing/Billing.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import './Billing.css';
 import Invoice from '../Invoice/Invoice';
+import DeliveryForm from '../DeliveryForm/DeliveryForm';
 
-const Billing = () => {
-  return (
-    <div className='billing'>
-      <div className='billing-front'>
-        <div className='invoice-details'>
-          <Invoice />
-        </div>
-        <div>
-          <button className='invoice__payment-button'>Process order</button>
+class Billing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFlipBackClick = this.handleFlipBackClick.bind(this);
+    this.handleFlipFrontClick = this.handleFlipFrontClick.bind(this);
+    this.state = { rotate: 'flip-front' };
+  }
+  handleFlipBackClick() {
+    this.setState({ rotate: 'flip-back' });
+  }
+  handleFlipFrontClick() {
+    this.setState({ rotate: 'flip-front' });
+  }
+
+  render() {
+    return (
+      <div className='billing flip-card'>
+        <div className={`flip-card-inner ${this.state.rotate}`}>
+          <div className='billing-front flip-card-front'>
+            <div className='invoice-details'>
+              <Invoice />
+            </div>
+            <div>
+              <button
+                className='invoice__payment-button'
+                onClick={this.handleFlipBackClick}
+              >
+                Process order
+              </button>
+            </div>
+          </div>
+          <div className='billing-rear flip-card-back'>
+            <div className='form'>
+              <DeliveryForm />
+            </div>
+            <div>
+              <button
+                className='invoice__payment-button'
+                onClick={this.handleFlipFrontClick}
+              >
+                Done
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Billing;
