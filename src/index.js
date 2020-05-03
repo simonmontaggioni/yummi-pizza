48e45b6d64b9442a3f33451b3e3b6abdd41fb86a
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-03 12:59:22 -0400

    feat: Set up the initial config for use redux and redux dev tools.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import reducer from './reducers';
+import initialState from './store/store';
+
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
+const composeEnhancers =
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+  compose;
+
+const store = createStore(reducer, initialState, composeEnhancers);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
