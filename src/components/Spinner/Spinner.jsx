5918f711764386dd57803c13530398047bed237c
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-03 10:32:01 -0400

    feat: Create Spinner component.

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class='lds-spinner'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Spinner;
