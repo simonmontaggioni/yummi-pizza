04011201ae33feece5ae95dc025ac8d6fca7df84
simon.montaggioni <simon.montaggioni@gmail.com>
2020-05-03 21:11:30 -0400

    feat: Connect the CartDetail component to the store.

--- src/components/CartDetail/CartDetail.jsx
+++ src/components/CartDetail/CartDetail.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './CartDetail.css';
 import CartItem from '../CartItem/ CartItem';
 
-const CartDetail = () => {
+const CartDetail = (props) => {
+  const cartItemsList = props.cartItems.map((cartItem, index) => (
+    <CartItem
+      key={cartItem.item.title + index.toString()}
+      item={cartItem.item}
+    />
+  ));
   return (
     <div className='cart-detail'>
       <div className='cart-detail__list-container'>
-        <ul className='cart-detail__list'>
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
-        </ul>
+        <ul className='cart-detail__list'>{cartItemsList}</ul>
       </div>
     </div>
   );
 };
 
-export default CartDetail;
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state.cartItems,
+  };
+};
+
+export default connect(mapStateToProps, null)(CartDetail);
