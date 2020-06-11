34b566dc91294e4d681806441adebf8a9379353e
simon.montaggioni <simon.montaggioni@gmail.com>
2020-06-11 12:19:01 -0400

    feat: Implement input validation with regex in signup form.

--- src/components/SignUpBox/SignUpBox.jsx
+++ src/components/SignUpBox/SignUpBox.jsx
@@ -13,32 +13,81 @@ const SignUpBox = (props) => {
     passwordConfirmation: '',
   });
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(true);
+  const [errors, setErrors] = useState({
+    userName: userData.userName === false ? true : false,
+    email: userData.email === false ? true : false,
+    password: userData.password === false ? true : false,
+    passwordConfirmation:
+      userData.passwordConfirmation === false ? true : false,
+  });
 
   const handleUserNameInputChange = (event) => {
     const userName = event.target.value;
-    setUserData({ ...userData, userName: userName });
+    const regex = /[a-z]{4}/gim;
+    const isValidInput = validateString(event.target.value, regex);
+    if (isValidInput) {
+      setUserData({ ...userData, userName: userName });
+      setErrors({ ...errors, userName: false });
+    } else {
+      setErrors({ ...errors, userName: true });
+    }
   };
 
   const handleEmailInputChange = (event) => {
     const email = event.target.value;
-    setUserData({ ...userData, email: email });
+    const regex = /[\S]*@[\S]*\.[\S]{1,}/gim;
+    const isValidInput = validateString(event.target.value, regex);
+    if (isValidInput) {
+      setUserData({ ...userData, email: email });
+      setErrors({ ...errors, email: false });
+    } else {
+      setErrors({ ...errors, email: true });
+    }
   };
 
   const handlePasswordInputChange = (event) => {
     const password = event.target.value;
-    setUserData({ ...userData, password: password });
+    const regex = /[\S]{5}/gim;
+    const isValidInput = validateString(event.target.value, regex);
+    if (isValidInput) {
+      setUserData({ ...userData, password: password });
+      setErrors({ ...errors, password: false });
+    } else {
+      setErrors({ ...errors, password: true });
+    }
   };
 
   const handlePasswordConfirmationInputChange = (event) => {
     const password = event.target.value;
     if (password !== userData.password) {
-      setError(true);
+      setErrors({ ...errors, passwordConfirmation: true });
     } else {
-      setError(false);
+      setErrors({ ...errors, passwordConfirmation: false });
     }
 
     setUserData({ ...userData, passwordConfirmation: password });
+  };
+
+  const validateString = (inputString, regex) => {
+    const validation = inputString.match(regex);
+    if (validation !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const validateForm = () => {
+    return errors.userName ||
+      errors.email ||
+      errors.password ||
+      errors.passwordConfirmation ||
+      userData.userName === '' ||
+      userData.email === '' ||
+      userData.password === '' ||
+      userData.passwordConfirmation === ''
+      ? true
+      : false;
   };
 
   const handleCancelClick = () => {
@@ -82,7 +131,9 @@ const SignUpBox = (props) => {
             <form>
               <div className='signup-input-wrapper'>
                 <input
-                  className='signup-input'
+                  className={`signup-input ${
+                    errors.userName ? 'wrong-input' : ''
+                  }`}
                   type='text'
                   name='user'
                   id='user'
@@ -90,7 +141,9 @@ const SignUpBox = (props) => {
                   onChange={handleUserNameInputChange}
                 />
                 <input
-                  className='signup-input'
+                  className={`signup-input ${
+                    errors.email ? 'wrong-input' : ''
+                  }`}
                   type='email'
                   name='email'
                   id='email'
@@ -98,7 +151,9 @@ const SignUpBox = (props) => {
                   onChange={handleEmailInputChange}
                 />
                 <input
-                  className='signup-input'
+                  className={`signup-input ${
+                    errors.password ? 'wrong-input' : ''
+                  }`}
                   type='password'
                   name='password'
                   id='password'
@@ -107,7 +162,9 @@ const SignUpBox = (props) => {
                 />
                 {userData.password && (
                   <input
-                    className={`signup-input ${error ? 'wrong-input' : ''}`}
+                    className={`signup-input ${
+                      errors.passwordConfirmation ? 'wrong-input' : ''
+                    }`}
                     type='password'
                     name='password-confirmation'
                     id='password-confirmation'
@@ -131,6 +188,7 @@ const SignUpBox = (props) => {
           <button
             className='footer-card__signup-button footer-card__signup-button--signup'
             onClick={handleRegisterClick}
+            disabled={validateForm() ? true : false}
           >
             Register
           </button>
